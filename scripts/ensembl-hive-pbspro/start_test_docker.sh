30d46753d805759bf64b0119b4072af584a1f08a
Matthieu Muffato <muffato@ebi.ac.uk>
2018-03-07 16:41:03 +0000

    Added a hint about what to run

--- scripts/ensembl-hive-pbspro/start_test_docker.sh
+++ scripts/ensembl-hive-pbspro/start_test_docker.sh
@@ -4,5 +4,10 @@ HIVE_PBSPRO_LOCATION=$1
 EHIVE_LOCATION=$2
 DOCKER_NAME=${3:-ensemblorg/ensembl-hive-pbspro}
 
+echo "!!!!!!!!!!!!!!"
+echo "! To test the PBSPro meadow, run: prove -v '/repo/ensembl-hive/t/04.meadow/meadow-longmult.mt'"
+echo "!!!!!!!!!!!!!!"
+echo
+
 exec docker run -it -v "$EHIVE_LOCATION:/repo/ensembl-hive" -v "$HIVE_PBSPRO_LOCATION:/repo/ensembl-hive-pbspro" "$DOCKER_NAME"
 
